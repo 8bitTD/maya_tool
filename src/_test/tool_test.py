9147f04dd0f1f86e7239735e0d0762ea8a43bda2
8bit <freeman1218@gmail.com>
2021-04-14 16:48:38 +0900

    Add files via upload

---
+++ src/_test/tool_test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#ツールテスト
+import maya.cmds as cmds
+class tool_test(object):
+	def __init__(self):
+		self.win = u"tool_test"
+		if ( cmds.window(self.win,exists=True)):cmds.deleteUI(self.win)
+		win = cmds.window(self.win,tlb=True)
+		cmds.showWindow(win)
+tool_test()
